e77830f1233a6916da970bb3b8fd1e895cadec80
Adonis K <aklp08@gmail.com>
2015-01-11 19:00:27 +0200

    various changes to the code

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 _ = require('underscore');
-fs.require('fs');
+fs = require('fs');
 
-var input = process.argv[1];
-var output = process.argv[2];
+var input = process.argv[2];
+var output = process.argv[3];
 
 if (!input) {
 	input = './input.json';
@@ -16,16 +16,20 @@ if (!output) {
 
 fs.readFile(input, 'utf-8', function (err, data) {
 	if (err) {
-		throw new Error(e.code + ': ' + e.message);
+		throw new Error(err.message);
 	}
+
+	var result = _.indexBy(JSON.parse(data), function (item) {
+		return item.dept;
+	});
+
+
+	var formattedResult = JSON.stringify(result, null, 4);
+
+	fs.writeFile(output, formattedResult, function (err) {
+		if (err) {
+			throw new Error(err.message);
+		}
+	});
 });
 
-var result = _.indexBy(arr, function (item) {
-	return item.dept;
-});
-
-fs.readFile(output, result, function (err) {
-	if (err) {
-		throw new Error(e.code + ': ' + e.message);
-	}
-});
