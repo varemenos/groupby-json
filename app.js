d58c78ef0595c6a8d469fa5158c5258159add16c
Adonis K <aklp08@gmail.com>
2015-01-11 18:47:37 +0200

    added required files and basic functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+_ = require('underscore');
+fs.require('fs');
+
+var input = process.argv[1];
+var output = process.argv[2];
+
+if (!input) {
+	input = './input.json';
+}
+
+if (!output) {
+	output = './output.json';
+}
+
+fs.readFile(input, 'utf-8', function (err, data) {
+	if (err) {
+		throw new Error(e.code + ': ' + e.message);
+	}
+});
+
+var result = _.indexBy(arr, function (item) {
+	return item.dept;
+});
+
+fs.readFile(output, result, function (err) {
+	if (err) {
+		throw new Error(e.code + ': ' + e.message);
+	}
+});
